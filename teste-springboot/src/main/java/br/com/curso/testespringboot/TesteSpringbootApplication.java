291ba20d55cf336618ff580499ffc18220ca4c26
erikancardoso <erikancardoso@outlook.com>
2023-02-01 02:14:42 -0300

    configuracao

---
+++ teste-springboot/src/main/java/br/com/curso/testespringboot/TesteSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.curso.testespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteSpringbootApplication.class, args);
+	}
+
+}
---
+++ teste-springboot/src/test/java/br/com/curso/testespringboot/TesteSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.curso.testespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
